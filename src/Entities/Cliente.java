a0a548c5df0f3e11132cb4ff3537df6224693363
Filipealves0 <filipe.alves@dcx.ufpb.br>
2025-08-26 19:17:08 -0300

    Criação de classes

---
+++ src/Entities/Cliente.java
@@ -0,0 +1,59 @@
+package Entities;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+    private Integer telefone;
+    private String email;
+    private String endereco;
+
+
+    public Cliente(Integer id, String nome, Integer telefone, String email, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ src/Entities/Veiculo.java
@@ -0,0 +1,48 @@
+package Entities;
+
+public class Veiculo {
+
+    private Cliente cliente;
+    private String placa;
+    private String modelo;
+    private Integer ano;
+
+    public Veiculo(Cliente cliente, String placa, String modelo, Integer ano) {
+        this.cliente = cliente;
+        this.placa = placa;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
